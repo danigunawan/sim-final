1eaa879d33df360ee91320693fd09ffc6cc2152c
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-01-18 07:54:27 +0700

    Initial Commit & Crud User

---
+++ resources/assets/js/router.js
@@ -0,0 +1,17 @@
+//DASHBOARD
+import DashboardIndex from './components/dashboard/DashboardIndex.vue';
+import UserIndex from './components/user/UserIndex.vue';
+import UserCreate from './components/user/UserCreate.vue';
+import UserEdit from './components/user/UserEdit.vue';
+ const routes = [
+     {
+        path: '/',
+        component: DashboardIndex,
+        name : 'indexDashboard'
+     },
+     { path: '/user', component: UserIndex,name: 'IndexUser' },
+     { path: '/user/create', component: UserCreate,name: 'CreateUser' },
+     { path: '/user/edit/:id', component: UserEdit,name: 'EditUser' }
+ ];
+
+ export default routes;
